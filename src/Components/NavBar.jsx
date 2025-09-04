92a8aeb7fa17ced76ea85b234e57b7022f879f05
rahulgope45 <rahuleile456@gmail.com>
2025-09-05 01:04:00 +0530

    NavBar Color update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,17 +14,17 @@ function NavBar() {
 
       {/* Menu Links */}
       <div className='flex h-16 items-center gap-6 text-gray-700 font-medium'>
-        <p className='hover:text-blue-500 cursor-pointer'>Home</p>
-        <p className='hover:text-blue-500 cursor-pointer'>Buy</p>
-        <p className='hover:text-blue-500 cursor-pointer'>Rent</p>
-        <p className='hover:text-blue-500 cursor-pointer'>Sell</p>
-        <p className='hover:text-blue-500 cursor-pointer'>About Us</p>
-        <p className='hover:text-blue-500 cursor-pointer'>Contact Us</p>
+        <p className='hover:text-blue-700 cursor-pointer'>Home</p>
+        <p className='hover:text-blue-700 cursor-pointer'>Buy</p>
+        <p className='hover:text-blue-700 cursor-pointer'>Rent</p>
+        <p className='hover:text-blue-700 cursor-pointer'>Sell</p>
+        <p className='hover:text-blue-700 cursor-pointer'>About Us</p>
+        <p className='hover:text-blue-700 cursor-pointer'>Contact Us</p>
       </div>
 
       {/* Login Button */}
       <div>
-        <button className='flex items-center gap-2 px-6 py-2 rounded-full bg-blue-500 text-white font-semibold hover:bg-blue-600 transition'>
+        <button className='flex items-center gap-2 px-6 py-2 rounded-full bg-blue-900 text-white font-semibold hover:bg-blue-600 transition'>
           Login / Register 
           <i className="bi bi-arrow-right-circle text-lg"></i>
         </button>
