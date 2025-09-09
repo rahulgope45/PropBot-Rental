45d16409528b72295accd7d483e81236f8421c38
rahulgope45 <rahuleile456@gmail.com>
2025-09-09 14:57:14 +0530

    Buy page banner section is native now

--- src/Pages/Buy.jsx
+++ src/Pages/Buy.jsx
@@ -3,6 +3,7 @@ import banner2 from '/banner2.png'
 import Geo1 from '/Geo1.png'
 import search1 from '/search1.png'
 import Featuredbuy from '../Components/Featuredbuy'
+import nativebanner from '/nativebanner.jpg'
 
 function Buy() {
   return (
@@ -15,35 +16,36 @@ function Buy() {
             <img
               src={banner2}
               alt="banner"
-              className="w-full h-full object-cover rounded-xl z-0"
+              className="hidden sm:block w-full h-full object-cover rounded-xl z-0"
             />
-            
+            <img
+              src={nativebanner}
+              alt="native banner"
+              className="block sm:hidden w-full h-full object-cover rounded-xl z-0"
+            />
 
-            <div className="absolute left-[500px] transform -translate-x-1/2 bottom-20 z-20 flex  ">
+
+            <div className="absolute left-1/2 sm:left-[500px] transform -translate-x-1/2 bottom-10 sm:bottom-20 z-20 flex justify-center">
               {/* Search box overlay - centered, sits above image */}
-              <div className=" w-[90%] max-w-[820px]">
-                <div className=" gap-4  px-6 py-4 h-[72px] w-[900px] ">
-                  <p className='text-white text-5xl font-bold'>
+              <div className="w-[90%] max-w-[820px]">
+                <div className="px-2 sm:px-6 py-3 sm:py-4 w-full sm:w-[900px]">
+                  <p className="text-white text-2xl sm:text-5xl font-bold text-center sm:text-left">
                     Featured Properties For Sale
                   </p>
-                  <p className='text-white text-[20px]xl'>
-                  Discover, Buy, or Rent Verified Properties with Ease.
+                  <p className="text-white text-base text-1xl sm:text-[20px] mt-2 text-center sm:text-left">
+                    Discover, Buy, or Rent Verified Properties with Ease.
                   </p>
                 </div>
               </div>
-
-              
-             
-
             </div>
             {/* background blur */}
-             <div className="absolute bottom-0 left-0 w-full h-[208px] w-[1328px] bg-gradient-to-t from-black/10 to-transparent backdrop-blur-[1.8px] rounded-xl"></div>
+            <div className="absolute bottom-0 left-0 w-full h-[208px] w-[1328px] bg-gradient-to-t from-black/10 to-transparent backdrop-blur-[1.8px] rounded-xl"></div>
 
 
           </div>
         </div>
         <div>
-          <Featuredbuy/>
+          <Featuredbuy />
         </div>
 
 
